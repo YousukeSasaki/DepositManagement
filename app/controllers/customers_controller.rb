6ce24d47d3ac94e469fa538963f914d8067dee0a
YosukeSasaki <yosaprog@gmail.com>
2019-06-16 13:27:47 +0900

    Update customers_controller.rb

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,8 @@
 class CustomersController < ApplicationController
+  layout "main_application"
+
   def index
+    @customers = Customer.all
   end
 
   def new
@@ -7,5 +10,20 @@ class CustomersController < ApplicationController
   end
 
   def create
+    customer = Customer.new(customer_params)
+    binding.pry
+    if customer.save
+      flash[:notice] = "登録が完了しました"
+      redirect_to customers_path
+    else
+      flash[:alert] = '登録が失敗しました。必須項目を確認してください。'
+      redirect_to new_customer_path
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :kana, :sex, :birthday, :phone_number, :cell_phone_number, :emergency_phone_number, :postal_code, :prefecture_id, :city, :address, :building, :start_date, :image)
   end
 end
