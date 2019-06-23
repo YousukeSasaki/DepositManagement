9aa90d55fc9b5d8a425e6716971967377680c874
YosukeSasaki <yosaprog@gmail.com>
2019-06-23 19:09:25 +0900

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,13 +6,13 @@ class Customer < ApplicationRecord
 
   validates :name, length: {in: 1..10}, presence: true
   validates :kana, length: {in: 1..10}, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-  validates :phone_number, length: {in: 1..13}
-  validates :cell_phone_number, length: {in: 1..13}
-  validates :emergency_phone_number, length: {in: 1..13}
-  validates :postal_code, length: {in: 1..8}
-  validates :city, length: {in: 1..50}
-  validates :address, length: {in: 1..50}
-  validates :building, length: {in: 1..50}
+  validates :phone_number, length: {in: 0..13}
+  validates :cell_phone_number, length: {in: 0..13}
+  validates :emergency_phone_number, length: {in: 0..13}
+  validates :postal_code, length: {in: 0..8}
+  validates :city, length: {in: 0..50}
+  validates :address, length: {in: 0..50}
+  validates :building, length: {in: 0..50}
 
   mount_uploader :image, ImageUploader
 
