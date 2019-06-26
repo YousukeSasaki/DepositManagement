0774f3d5368f970400b0403657b7b69765b821d2
YosukeSasaki <yosaprog@gmail.com>
2019-06-26 17:08:49 +0900

    Update institution.rb

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -2,7 +2,7 @@ class Institution < ApplicationRecord
   has_many :customers, through: :customer_institutions
   has_many :customer_institutions
   has_many :transactions
-  belongs_to :institution_types
+  belongs_to :institution_type
   belongs_to :prefecture
 
   mount_uploader :image, ImageUploader
