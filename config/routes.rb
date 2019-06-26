bdb5b89266357b62584a7b142843d164d491ceac
YosukeSasaki <yosaprog@gmail.com>
2019-06-26 09:52:04 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,26 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources 'users', only: :show
-  resources 'transactions'
-  resources 'institutions'
+
+  resources 'transactions', except: :new do
+    collection do
+      get 'customer_choice'
+      get 'new_choice'
+    end
+  end
+  resource 'transactions', path: 'transactions/c:customer_id', only: :new
+  resource 'transactions', action: 'customer', path: 'transactions/c:customer_id', only: :show, as: 'customer_transactions'
+
+  namespace :api do
+    resources 'transactions', only: [:number, :select], defaults: { format: 'json' } do
+      collection do
+        get 'number'
+        get 'select'
+      end
+    end
+  end
+
   resources 'customers'
+  resources 'institutions'
   resources 'subjects'
 end
