a94b1b945fb23e78fe7fb827cc80514105799341
YosukeSasaki <yosaprog@gmail.com>
2019-06-27 14:31:09 +0900

    Update customers_helper.rb

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -5,11 +5,6 @@ module CustomersHelper
     return sex
   end
 
-  def full_address(cus)
-    address = cus.prefecture.name + cus.city + cus.address + cus.building
-    return address
-  end
-
   def time_change(time)
     str = time.to_s
     correct_time = str.gsub(/\+0900/, '')
