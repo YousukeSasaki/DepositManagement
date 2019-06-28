bdd3adb0b44909aac0076883133bf75a5770d57e
YosukeSasaki <yosaprog@gmail.com>
2019-06-28 13:23:07 +0900

    Update 20190611145732_devise_create_users.rb

--- db/migrate/20190611145732_devise_create_users.rb
+++ db/migrate/20190611145732_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       ## Recoverable
       t.string   :reset_password_token
-      t.datetime :rese_password_sent_at
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
