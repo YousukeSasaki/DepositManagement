bff08b451f5822f86df15bae252214a04204306d
YosukeSasaki <yosaprog@gmail.com>
2019-06-28 18:52:39 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require './db/seeds/prefecture.rb'
 require './db/seeds/subject.rb'
-require './db/seeds/institution.rb'
 require './db/seeds/institution_type.rb'
+require './db/seeds/institution.rb'
 
 # 本番環境では実行しない
 require './db/seeds/customer.rb'
+require './db/seeds/balance.rb'
+require './db/seeds/transaction.rb'
