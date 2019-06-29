275994be644d330a8efaecab88eed73d7794bd43
YosukeSasaki <yosaprog@gmail.com>
2019-06-29 18:02:43 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   root 'users#index'
   resources 'users', only: :show
 
+  resource 'transactions', path: 'transactions/c_:customer_id', only: :new
+  resource 'transactions', action: 'customer', path: 'transactions/c_:customer_id', only: :show, as: 'customer_transactions'
   resources 'transactions', except: :new do
     collection do
       get 'customer_choice'
       get 'new_choice'
     end
   end
-  resource 'transactions', path: 'transactions/c:customer_id', only: :new
-  resource 'transactions', action: 'customer', path: 'transactions/c:customer_id', only: :show, as: 'customer_transactions'
 
   namespace :api do
     resources 'transactions', only: [:number, :select], defaults: { format: 'json' } do
