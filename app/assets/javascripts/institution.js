d6bf98fbc611090a502ca7f58bb724b6a613c4fb
YosukeSasaki <yosaprog@gmail.com>
2019-07-02 16:53:27 +0900

    不要な記述削除　viewの微調整

--- app/assets/javascripts/institution.js
+++ app/assets/javascripts/institution.js
@@ -1,12 +1,12 @@
 $(function() {
 
     // 画像選択プレビュー
-    $(document).on('change', '.cus-new--right__image input[type="file"]', function(e) {
+    $(document).on('change', '.ins-new--right__image input[type="file"]', function(e) {
         e.preventDefault
         var file = e.target.files[0],
             reader = new FileReader(),
-            $default = $(".cus-new--right__default")
-            $preview = $(".cus-new--right__preview")
+            $default = $(".ins-new--right__default")
+            $preview = $(".ins-new--right__preview")
 
         // 画像ファイル以外の場合は何もしない
         if(file.type.indexOf("image") < 0) {
@@ -30,23 +30,5 @@ $(function() {
             };
         })(file);
         reader.readAsDataURL(file);
-    });
-
-    $(document).on('keyup', '.cus-new--left__amount--input > input', function(){
-        var input = ".cus-new--left__amount--input > input";
-            amount = $(input).val();
-            str = String(amount); 
-            amount_str = str.replace(/(\d)(?=(\d\d\d)+$)/g, '$1,');
-        $(".cus-new--left__amount--number > p").text("¥ " + amount_str);
-    });
-
-    $(document).on('input', '#customer_amount', function() {
-        var value = this.value;
-        var length = this.value.length;
-        var maxlength = $(this).attr('maxlength');
-        
-        if (length > maxlength) {
-            this.value = value.slice(0, maxlength);
-        }
     });
 });
