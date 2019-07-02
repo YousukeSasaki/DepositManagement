204656e9e6f4cfccdd0c35740422744c8dda9fdb
YosukeSasaki <yosaprog@gmail.com>
2019-07-03 00:52:15 +0900

    EnterキーでTab送り

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -20,4 +20,53 @@ $(function() {
         $(".delete__modal").addClass("hidden");
     });
 
+    // inputタグにおいてEnterキーをTabキーと同様に機能させる
+    function fEnterChangeTab(){	
+    
+        var oObject = ".wrapper :input:not(:button):not(:hidden)";
+        
+        $(oObject).keypress(function(e) { 
+        var c = e.which ? e.which : e.keyCode;
+        if (c == 13) {  
+            var index = $(oObject).index(this);
+            var cNext = "";
+            var nLength = $(oObject).length;
+            for(i=index;i<nLength;i++){
+                cNext = e.shiftKey ? ":lt(" + index + "):last" : ":gt(" + index + "):first";
+                if ($(oObject + cNext).attr("readonly") == "readonly") {
+                    if (e.shiftKey) index--; 
+                    else index++;
+                }
+                else if ($(oObject + cNext).prop("disabled") == true) {
+                    if (e.shiftKey) index--;
+                    else index++;
+                }
+                else break;
+            }
+            if (index == nLength - 1) {
+                if (! e.shiftKey){
+                    cNext = ":eq(1)";
+                }
+            }
+            if (index == 0) {
+                if (e.shiftKey) {
+                    cNext = ":eq(" + (nLength - 1) + ")";
+                }
+            }
+            $(oObject + cNext).focus();
+            e.preventDefault() 
+        }  
+        }); 
+    } 
+    
+    if(window.attachEvent){
+        window.attachEvent('onload',fEnterChangeTab);
+    }
+    else if (window.opera){
+        window.addEventListener('load',fEnterChangeTab,false);
+    }
+    else {
+        window.addEventListener('load',fEnterChangeTab,false);
+    }
+
 });
